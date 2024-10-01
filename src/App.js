b843a58a0ed1224dc349df37d09e539be552982a
dehemi weerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2024-10-01 13:01:33 +0530

    modified the pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProtectedRoutes from './utills/ProtectedRoutes';
 import Item from './pages/Item/Item';
 import Home from './pages/Home/Home';
 import Category from './pages/Category/Category';
+import Stock from './pages/Stock/Stock';
 function App() {
   return (
     <div className="App">    
@@ -15,6 +16,7 @@ function App() {
         <Route path='/item' element={<Item/>}/>
         <Route path='/home' element={<Home/>}/>
         <Route path='/category' element={<Category/>}/>
+        <Route path='/stock' element={<Stock/>}/>
         </Route>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
---
+++ src/pages/Stock/Stock.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stock = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Stock
